a9656b314c77a4f17fb7fc26fc45962edac5eca5
leessangz <940034083@qq.com>
2017-06-15 14:21:22 +0800

    Site updated: 2017-06-15 14:21:20

---
+++ js/src/duoshuoshuo.js
@@ -0,0 +1,17 @@
+<!--崩溃欺骗-->
+var OriginTitile = document.title;
+ var titleTime;
+ document.addEventListener('visibilitychange', function () {
+     if (document.hidden) {
+         $('[rel="icon"]').attr('href', "/img/TEP.ico");
+         document.title = '╭(°A°`)╮ 看我啊！看我！';
+         clearTimeout(titleTime);
+     }
+     else {
+         $('[rel="icon"]').attr('href', "/favicon.ico");
+         document.title = '(ฅ>ω<*ฅ) 对！就是看我！' + OriginTitile;
+         titleTime = setTimeout(function () {
+             document.title = OriginTitile;
+         }, 2000);
+     }
+ });
